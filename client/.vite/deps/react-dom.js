ae93e8768b5ffbc4bb218379308312c2656f4953
sushant pokhrel <sushant.805421@cct.tu.edu.np>
2025-06-15 19:40:31 +0545

    vite caching for build

--- client/.vite/deps/react-dom.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_react_dom
-} from "./chunk-4DCA4LZO.js";
-import "./chunk-Q537VV3H.js";
-import "./chunk-DP4XHQAG.js";
-export default require_react_dom();
--- client/.vite/deps/react_jsx-runtime.js
+++
@@ -1,6 +0,0 @@
-import {
-  require_jsx_runtime
-} from "./chunk-KR7A7I3V.js";
-import "./chunk-Q537VV3H.js";
-import "./chunk-DP4XHQAG.js";
-export default require_jsx_runtime();
